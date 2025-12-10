bd525e2d01cd71012f22da9a9f29351bfcb1e0e2
Dariusz Majnert <dariusz.majnert@gmail.com>
2025-12-10 11:28:30 +0100

    fix: delete invalid import

--- genesis/src/index.ts
+++ genesis/src/index.ts
@@ -1,5 +1,5 @@
 export { GenesisBundler } from "./bundler";
-export type { AppManifest, BundleOptions, BundleResult, ExtractOptions } from "./bundler";
+export type { BundleOptions, BundleResult, ExtractOptions } from "./bundler";
 export type { Compressor } from "./compression";
 export { ZstdCodecCompressor } from "./compression";
 
