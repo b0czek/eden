9aab236068d27b0a2095fc4940eb96c28814a621
Dariusz Majnert <dariusz.majnert@gmail.com>
2026-01-19 10:48:14 +0100

    feat(sdk): enforce min window size

--- packages/sdk/src/Eden.ts
+++ packages/sdk/src/Eden.ts
@@ -127,6 +127,8 @@ export class Eden {
     this.mainWindow = new BrowserWindow({
       width: windowConfig.width || 1280,
       height: windowConfig.height || 800,
+      minWidth: 800,
+      minHeight: 600,
       title: windowConfig.title || "Eden",
       webPreferences: {
         nodeIntegration: false,
